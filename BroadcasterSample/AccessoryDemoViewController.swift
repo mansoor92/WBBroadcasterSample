917203c364d99a77a41c62aaf9f0c260b5496293
Mansoor Ali <mali92390@gmail.com>
2023-05-15 15:58:02 +0500

    updated broadcasting mechanism

--- BroadcasterSample/AccessoryDemoViewController.swift
+++ BroadcasterSample/AccessoryDemoViewController.swift
@@ -22,19 +22,35 @@ enum MessageId: UInt8 {
     case stop = 0xC
 }
 
-class AccessoryDemoViewController: UIViewController {
+class ViewController: UIViewController {
     
     private let broadcaster = Broadcaster()
     
-    @IBOutlet weak var connectionStateLabel: UILabel!
-    @IBOutlet weak var uwbStateLabel: UILabel!
-    @IBOutlet weak var infoLabel: UILabel!
-    @IBOutlet weak var distanceLabel: UILabel!
-    @IBOutlet weak var actionButton: UIButton!
+    @IBOutlet weak var messageLabel: UILabel!
+    @IBOutlet weak var readValueLabel: UILabel!
+    @IBOutlet weak var writeValueLabel: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        broadcaster.delegate = self
         broadcaster.start()
+    }
+}
+
+extension ViewController: BroadcasterDelegate {
+    
+    func didStartAdvertising() {
+        messageLabel.text = "Advertising Data"
+        print("Started Advertising")
+    }
+    
+    func didReceiveRead(_ value: String) {
+        messageLabel.text = "Data getting Read"
+        readValueLabel.text = value
+    }
+    
+    func didReceiveWrite(_ value: String) {
+        messageLabel.text = "Writing Data"
+        writeValueLabel.text = value
     }
 }
--- BroadcasterSample/BluetoothSupport/Broadcaster.swift
+++ BroadcasterSample/BluetoothSupport/Broadcaster.swift
@@ -11,25 +11,26 @@ import CoreBluetooth
 import UIKit
 import os
 
-struct TransferService {
-    static let serviceUUID = CBUUID(string: "6E400001-B5A3-F393-E0A9-E50E24DCCA9E")
-//    static let rxCharacteristicUUID = CBUUID(string: "6E400002-B5A3-F393-E0A9-E50E24DCCA9E")
-//    static let txCharacteristicUUID = CBUUID(string: "6E400003-B5A3-F393-E0A9-E50E24DCCA9E")
-    static let heartRateMeasurementUUID = CBUUID(string: "2A37")
-}
-
 enum BluetoothLECentralError: Error {
     case noPeripheral
+}
+
+protocol BroadcasterDelegate: AnyObject {
+    func didStartAdvertising()
+    func didReceiveRead(_ value: String)
+    func didReceiveWrite(_ value: String)
 }
 
 class Broadcaster: NSObject {
   
     private var bluetoothReady = false
     private var shouldStartWhenReady = false
-    private var service: CBMutableService?
     private var peripheralManager: CBPeripheralManager!
-    private var characteristic: CBMutableCharacteristic!
+    private var service: CBUUID!
+    private let value = "AD34E"
     private let logger = os.Logger(subsystem: "com.example.apple-samplecode.NINearbyAccessorySample", category: "Broadcaster")
+    
+    weak var delegate: BroadcasterDelegate?
     
     override init() {
         super.init()
@@ -39,30 +40,35 @@ class Broadcaster: NSObject {
     
     func start() {
         if bluetoothReady {
-            createService()
+            addService()
         } else {
             shouldStartWhenReady = true
         }
     }
     
-    private func createService() {
-//        let bytes = [0x1,0x2,0x3,0xA,0xB,0xC]
-//        let data = Data.init(bytes: bytes, count: bytes.count)
-        let service = CBMutableService(type: TransferService.serviceUUID, primary: true)
-//        let characteristic = CBMutableCharacteristic(type: TransferService.txCharacteristicUUID, properties: [.read, .notify], value: nil, permissions: [.readable])
-        let heartRateMeasurementCharacteristic = CBMutableCharacteristic(type: TransferService.heartRateMeasurementUUID, properties: [.notify], value: nil, permissions: [.readable])
-        service.characteristics = [heartRateMeasurementCharacteristic]
-        peripheralManager?.add(service)
-        self.service = service
-        self.characteristic = heartRateMeasurementCharacteristic
+    private func addService() {
+        let valueData = value.data(using: .utf8)
+        let myChar1 = CBMutableCharacteristic(type: CBUUID(nsuuid: UUID()), properties: [.notify, .write, .read], value: nil, permissions: [.readable, .writeable])
+          let myChar2 = CBMutableCharacteristic(type: CBUUID(nsuuid: UUID()), properties: [.read], value: valueData, permissions: [.readable])
+        
+        service = CBUUID(nsuuid: UUID())
+        let myService = CBMutableService(type: service, primary: true)
+        // 3. Add characteristics to the service
+        myService.characteristics = [myChar1, myChar2]
+        // 4. Add service to peripheralManager
+        peripheralManager.add(myService)
+        // 5. Start advertising
+        startAdvertising()
+    }
+    
+    private func startAdvertising() {
+        peripheralManager.startAdvertising([CBAdvertisementDataLocalNameKey : "BLEPeripheralApp", CBAdvertisementDataServiceUUIDsKey : [service]])
+        delegate?.didStartAdvertising()
     }
     
     private func cleanUp() {
-        if let service = service {
-            peripheralManager.remove(service)
-            self.service = nil
-            characteristic = nil
-        }
+        peripheralManager.removeAllServices()
+        self.service = nil
     }
     
     private func sendData(_ data: Data, toCharacteristic characteristic: CBMutableCharacteristic) throws {
@@ -86,7 +92,7 @@ extension Broadcaster: CBPeripheralManagerDelegate {
     func peripheralManagerDidUpdateState(_ peripheral: CBPeripheralManager) {
         switch peripheral.state {
         case .poweredOn:
-            createService()
+            addService()
         default: break
             
         }
@@ -99,12 +105,7 @@ extension Broadcaster: CBPeripheralManagerDelegate {
             start()
             return
         }
-        
-        let data: [String: Any] = [
-            CBAdvertisementDataLocalNameKey: "Mansoor Device",
-            CBAdvertisementDataServiceUUIDsKey: [TransferService.serviceUUID]
-        ]
-        peripheralManager?.startAdvertising(data)
+//        startAdvertising()
     }
     
     func peripheralManagerDidStartAdvertising(_ peripheral: CBPeripheralManager, error: Error?) {
@@ -116,38 +117,33 @@ extension Broadcaster: CBPeripheralManagerDelegate {
     }
     
     func peripheralManager(_ peripheral: CBPeripheralManager, central: CBCentral, didSubscribeTo characteristic: CBCharacteristic) {
-        guard characteristic.uuid == TransferService.heartRateMeasurementUUID, let mutableCharacteristic = self.characteristic else { return }
         logger.log("characteristic: \(characteristic.uuid) subscribed")
-        
-//        let data = "Initial value".data(using: .utf8)!
-//        var msg = Data([MessageId.configureAndStart.rawValue])
-//        msg.append(data)
-        
-        /*
-        let heartRateValue: UInt16 = 60
-        let sensorContactStatus: UInt8 = 2 // "not supported"
-        let heartRateMeasurementData = Data([UInt8(heartRateValue & 0xFF), UInt8(heartRateValue >> 8), sensorContactStatus])*/
-        
-        // Heart Rate Measurement Value (16-bit unsigned integer)
-        let heartRateValue: UInt16 = 60
-        // Sensor Contact Status (bits 0-2)
-        let sensorContactStatus: UInt8 = 0b00000100 // "supported but contact is not detected"
-        // Energy Expended (8-bit unsigned integer)
-        let energyExpended: UInt8 = 0
-        // Heart Rate Measurement Data (16-bit HRM value + 8-bit Sensor Contact Status + 8-bit Energy Expended)
-        let heartRateMeasurementData = Data([UInt8(heartRateValue & 0xFF), UInt8(heartRateValue >> 8), sensorContactStatus, energyExpended])
-
-        
-        do {
-            try sendData(heartRateMeasurementData, toCharacteristic: mutableCharacteristic)
-        } catch {
-            print(error)
-        }
     }
     
     func peripheralManager(_ peripheral: CBPeripheralManager, central: CBCentral, didUnsubscribeFrom characteristic: CBCharacteristic) {
-        guard characteristic.uuid == TransferService.heartRateMeasurementUUID else { return }
         logger.log("characteristic: \(characteristic.uuid) unsubscribed")
+    }
+    
+    func peripheralManager(_ peripheral: CBPeripheralManager, didReceiveRead request: CBATTRequest) {
+        delegate?.didReceiveRead(value)
+    }
+    
+    func peripheralManager(_ peripheral: CBPeripheralManager, didReceiveWrite requests: [CBATTRequest]) {
+        
+        guard let value = requests.first?.value else { return }
+        delegate?.didReceiveWrite(value.hexEncodedString())
+    }
+}
+
+extension Data {
+    struct HexEncodingOptions: OptionSet {
+        let rawValue: Int
+        static let upperCase = HexEncodingOptions(rawValue: 1 << 0)
+    }
+    
+    func hexEncodedString(options: HexEncodingOptions = []) -> String {
+        let format = options.contains(.upperCase) ? "%02hhX" : "%02hhx"
+        return map { String(format: format, $0) }.joined()
     }
 }
     
